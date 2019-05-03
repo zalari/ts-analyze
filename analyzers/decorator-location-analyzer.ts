fb639b05cce9cf70dfef153c37ba33590940eb06
Christian Klauß <christian.klauss@zalari.de>
2019-05-03 12:38:57 +0200

    Refactor Message Location Analyzer to generic Decorator Location Analyzer with parameter

--- analyzers/decorator-location-analyzer.ts
+++ analyzers/decorator-location-analyzer.ts
@@ -1,37 +1,40 @@
-import { RepoAnalyzerBase, RepoAnalysisContext, RepoAnalyzerResultBase, CodeWalkerDataResult } from "../src/api";
+import { RepoAnalyzerWithOptionsBase, RepoAnalysisContext, RepoAnalyzerResultBase } from "../src/api";
 import { DecoratorFinder } from "../walkers/decorator-finder";
 import { CodeWalkerNodeResult } from "../src/classes/code-walker-node-result.class";
-import { ClassDeclaration, Node } from "ts-morph";
-import { PropertyAccessFinder } from "../walkers";
-import { PropertyAccessFinderResult } from "../walkers/property-access-finder";
+import { Node } from "ts-morph";
 
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
 
-interface MessageLocationAnalyzerResult {
+
+interface DecoratorLocationAnalyzerResult {
     locations: {
         package: string,
         path: string
     }[]
 }
 
-export class MessageLocationAnalyzer extends RepoAnalyzerBase<MessageLocationAnalyzerResult> {
-  private decoratedNodes: Node[] = [];
+interface DecoratorLocationAnalyzerOptions {
+    decoratorName: string;
+}
 
+export class DecoratorLocationAnalyzer extends RepoAnalyzerWithOptionsBase<DecoratorLocationAnalyzerResult, DecoratorLocationAnalyzerOptions> {
+  private decoratedNodes: Node[] = [];
 
   initialize(context: RepoAnalysisContext): void {
     const decoratorFinderOptions = DecoratorFinder.getDefaultOptions();
-    decoratorFinderOptions.decoratorName = 'Message';
+    decoratorFinderOptions.decoratorName = this.options.decoratorName;
 
     context.registerWalker(DecoratorFinder, (results: CodeWalkerNodeResult[]) => results.forEach(r => this.decoratedNodes.push(r.data)), decoratorFinderOptions);
   }
 
-  getResult(): RepoAnalyzerResultBase<MessageLocationAnalyzerResult> {
+  getResult(): RepoAnalyzerResultBase<DecoratorLocationAnalyzerResult> {
     const locations: { package: string, path: string}[] = [];
 
     this.decoratedNodes.forEach(node => {
         const packageJson = this.findPackageJson(node.getSourceFile().getFilePath());
+        
         if (packageJson) {
             locations.push({ package: packageJson.name, path: node.getSourceFile().getFilePath() })
         }
@@ -42,8 +45,8 @@ export class MessageLocationAnalyzer extends RepoAnalyzerBase<MessageLocationAna
   }
 
   private findPackageJson(filePath: string): any | null {
-    let currentDir = path.dirname(filePath);
-    var fsRoot = (os.platform() === "win32") ? process.cwd().split(path.sep)[0] : "/"
+    let currentDir = path.dirname(filePath).toLowerCase();
+    var fsRoot = (os.platform() === "win32") ? process.cwd().split(path.sep)[0] + '/' : "/"
 
     while (currentDir !== fsRoot) {
         const directoryContents = fs.readdirSync(currentDir);
