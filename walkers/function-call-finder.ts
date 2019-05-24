0fbe54429c1b8155f27bbc7cc4e040b33809786c
Christian Klauß <christian.klauss@zalari.de>
2019-05-24 12:17:23 +0200

    Extract method in FunctionCallFinder

--- walkers/function-call-finder.ts
+++ walkers/function-call-finder.ts
@@ -20,26 +20,33 @@ interface FunctionCallFinderResultData extends FunctionCallFinderTarget {
 export class FunctionCallFinder extends CodeWalkerBase<FunctionCallFinderOptions> {
     walk(sourceFile: SourceFile): void {
         const file = this.wrap(sourceFile);
-        
+
         file.getDescendantsOfKind(SyntaxKind.CallExpression).forEach(callExpression => {
             const identifier = callExpression.getLastChildByKind(SyntaxKind.Identifier);
-            
+
+            const { functionName, exportedFrom } = this.options;
+
             if (identifier) {
                 const identifierSymbol = this.languageService.getSymbolSafe(identifier);
-                
+
                 if (identifierSymbol) {
                     const firstDeclaration = identifierSymbol.getDeclarations()[0];
-                    
+
                     if (TypeGuards.isFunctionDeclaration(firstDeclaration)) {
                         const functionDeclaration = firstDeclaration as FunctionDeclaration;
 
-                        if (this.isExportConstraintSatisfied(functionDeclaration, this.options.exportedFrom) && this.options.functionName === functionDeclaration.getName()) {
+                        if (this.isFunctionNameConstraintSatisfied(functionDeclaration, functionName) &&
+                            this.isExportConstraintSatisfied(functionDeclaration, exportedFrom)) {
                             this.addResult(new FunctionCallFinderResult({ ...this.options, expression: this.languageService.attach(callExpression) as CallExpression }));
                         }
                     }
                 }
             }
         });
+    }
+
+    private isFunctionNameConstraintSatisfied(declaration: FunctionDeclaration, functionName: string) {
+        return functionName === declaration.getName();
     }
 
     private isExportConstraintSatisfied(declaration: FunctionDeclaration, exportName?: string) {
